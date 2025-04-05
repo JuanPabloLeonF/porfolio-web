177e0c21071c6ec35dbf3033f69c75e1502e18c1
Juan Pablo Leon <fonseca123papo@gmail.com>
2025-04-04 19:13:27 -0500

    empezando a estilizar la pages projects

--- src/app/pages/proyect/proyect.component.ts
+++ src/app/pages/proyect/proyect.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { SvgTecnologyComponent } from "../../components/svg-tecnology/svg-tecnology.component";
+import { tecnologies, TypeTecnology } from '../../../utils/models';
 
 @Component({
   selector: 'app-proyect',
-  imports: [],
+  imports: [SvgTecnologyComponent],
   templateUrl: './proyect.component.html',
   styleUrl: './proyect.component.css'
 })
 export class ProyectComponent {
-
+  protected tecnologiesList: TypeTecnology[] = tecnologies;
 }
