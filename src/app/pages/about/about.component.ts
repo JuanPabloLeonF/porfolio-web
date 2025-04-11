83b8def32a15993a289438ec649928741a6af5e6
Juan Pablo Leon <fonseca123papo@gmail.com>
2025-04-11 16:54:42 -0500

    terminando la page about

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,47 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    NgClass, 
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  protected activateStylesButton: boolean = false;
+  protected activateStylesLink: boolean = false;
 
+  protected onHover(isHovered: boolean, element: string) {
+    if (isHovered) {
+      if (element === 'button') {
+        this.activateStylesLink = true;
+      } else if (element === 'link') {
+        this.activateStylesButton = true;
+      }
+    } else {
+      if (element === 'button') {
+        this.activateStylesLink = false;
+      } else if (element === 'link') {
+        this.activateStylesButton = false;
+      }
+    }
+  }
+
+  protected onScrollToTarget(event: Event): void {
+    event.preventDefault();
+    const target = document.getElementById('container-about-information');
+    const offset = 50;
+    if (target) {
+      const top = target.getBoundingClientRect().top + globalThis.pageYOffset - offset;
+      globalThis.scrollTo({
+        top,
+        behavior: 'smooth'
+      });
+    }
+  }
 }
